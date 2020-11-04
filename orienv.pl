5f6133e6161efdd4710cb627b1f891cb9a2844f1
Jon Starling <github@starling.org.uk>
2020-11-04 23:20:10 +0000

    fix JSON

--- orienv.pl
+++ orienv.pl
@@ -5,15 +5,19 @@ use warnings;
 
 print "Content-type: application/json; charset=iso-8859-1\n\n";
 
-print "[";
+print "{";
+
+my $comma = 0;
 
 foreach my $var (sort(keys(%ENV))) {
 	if ($var =~ /ORI_.*/i) {
 		my $val = $ENV{$var};
 		$val =~ s|\n|\\n|g;
 		$val =~ s|"|\\"|g;
-		print "{\"${var}\":\"${val}\"},";
+		if ($comma) { print ','; }
+		print "\"${var}\":\"${val}\"";
+		$comma = 1;
 	}
 }
 
-print "]";
+print "}";
